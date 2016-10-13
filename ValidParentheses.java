b11145621f8382fdf3710cf15c07bf764c6bcdb7
Ruiyang Liu <ruiyang@ruiyangs-MacBook-Pro.local>
2016-10-12 23:29:03 -0400

    Oct 12 2016'

---
+++ ValidParentheses.java
@@ -0,0 +1,23 @@
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if(s == null || s.length() == 0) {
+            return true;
+        }
+        
+        Stack<Character> stack = new Stack<>();
+
+        
+        for(int i=0; i<s.length(); i++) {
+            
+            if(!stack.isEmpty()&&(stack.peek() =='('&&s.charAt(i) == ')'||
+               stack.peek() =='['&&s.charAt(i) == ']'||
+               stack.peek() =='{'&&s.charAt(i) == '}')) {
+                   stack.pop();
+               }else {
+                   stack.push(s.charAt(i));
+               }
+        }
+        
+        return stack.isEmpty();
+    }
+}
